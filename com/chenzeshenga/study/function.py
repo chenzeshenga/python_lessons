f9ebb4773a6e3beb177464759364c18ff35ca34b
chenzeshenga <chenzeshenga@163.com>
2018-05-28 21:21:59 +0800

    polish

---
+++ com/chenzeshenga/study/function.py
@@ -0,0 +1,20 @@
+# @Time     :   2018-5-21 21:33
+# @Author   :   chenzeshenga
+# @Email    :   chenzeshenga@163.com
+# @File     :   function.py
+# @Software :   PyCharm
+
+
+def print_hello():
+    print("Hello World")
+
+
+print_hello()
+
+
+def test1(r):
+    s = r ** 2
+    print(s)
+
+
+test1(12)
