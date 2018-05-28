18d9e3ac06a92c6719f78bc2445a1291af199bfd
chenzeshenga <chenzeshenga@163.com>
2018-05-28 21:56:55 +0800

    local value

---
+++ com/chenzeshenga/study/localvarible.py
@@ -0,0 +1,14 @@
+# @Time     :   2018-5-28 21:23
+# @Author   :   chenzeshenga
+# @Email    :   chenzeshenga@163.com
+# @File     :   localvarible.py
+# @Software :   PyCharm
+
+
+def test1():
+    a = 200
+    print(a)
+
+
+# print(a)
+test1()
