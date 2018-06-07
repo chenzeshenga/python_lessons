85fd433458d0db0bcb1383ba925a7cb566d6e8cb
chenzeshenga <chenzeshenga@163.com>
2018-06-07 21:48:39 +0800

    recursion dir

--- com/chenzeshenga/study/recursion_dir_files.py
+++ com/chenzeshenga/study/recursion_dir_files.py
@@ -4,4 +4,34 @@
 # @File     :   recursion_dir_files.py
 # @Software :   PyCharm
 
-# TODO
+import os
+
+
+def recursion_dir(dir):
+    if os.path.isdir(dir):
+        for f in os.listdir(dir):
+            recursion_dir(f)
+    else:
+        print(os.path.abspath(dir))
+        if read_file(dir):
+            print(os.path.abspath(dir))
+
+
+def read_file(file):
+    flag = False
+    f = open(file)
+    while True:
+        line = f.readline()
+        if line == "":
+            break
+        elif "chenzeshenga" in line:
+            print(line)
+            flag = True
+            break
+    f.close()
+    return flag
+
+
+path = os.path.join("F:\\", "Idea_PyCharm")
+path = os.path.join(path, "com", "chenzeshenga", "study", "Condition.py")
+recursion_dir(path)
